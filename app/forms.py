0cbe6e087778762f8271e9cd36ea1489e44e2c40
[Gaia <[dzhayadzoy@gmail.com]>
2024-08-28 12:22:55 +0500

    lesson 8, added steps including class EmptyForm(FlaskForm)

--- app/forms.py
+++ app/forms.py
@@ -52,10 +52,6 @@ class EditProfileForm(FlaskForm):
             if user is not None:
                 raise ValidationError('Please use a different username.')
 
-class EmptyForm(FlaskForm):
-    submit = SubmitField('Submit')
 
-class PostForm(FlaskForm):
-    post = TextAreaField('Say something', validators=[
-        DataRequired(), Length(min=1, max=140)])
+class EmptyForm(FlaskForm):
     submit = SubmitField('Submit')
