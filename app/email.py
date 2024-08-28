356048c411fb8c835a25429b4de65d7efb45b508
[Gaia <[dzhayadzoy@gmail.com]>
2024-08-28 16:51:03 +0500

    lesson 10, send email func, link to reset password

--- app/email.py
+++ app/email.py
@@ -5,3 +5,5 @@ def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
+
+
