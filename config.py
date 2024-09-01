c37576c894b2431e03fc8eff568b103d8ed84099
[Gaia <[dzhayadzoy@gmail.com]>
2024-09-01 17:32:03 +0500

    lesson 13, babel instance and get_locale

--- config.py
+++ config.py
@@ -12,6 +12,7 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['dzhayadzoy@gmail.com']
+    LANGUAGES = ['en', 'es', 'rus']
 
     POSTS_PER_PAGE = 3
 
